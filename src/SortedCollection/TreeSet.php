2b8883506a03a26a0599f7878b2167a6b443c6ac
Christophe Demko <chdemko@gmail.com>
2015-03-17 23:39:31 +0100

    Changing year

--- src/SortedCollection/TreeSet.php
+++ src/SortedCollection/TreeSet.php
@@ -4,7 +4,7 @@
  * chdemko\SortedCollection\TreeSet class
  *
  * @author     Christophe Demko <chdemko@gmail.com>
- * @copyright  Copyright (C) 2012-2014 Christophe Demko. All rights reserved.
+ * @copyright  Copyright (C) 2012-2015 Christophe Demko. All rights reserved.
  *
  * @license    http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html The CeCILL B license
  *
