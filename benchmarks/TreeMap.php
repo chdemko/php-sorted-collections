0a581bcee95c9b4eb51a6853d8808e117859a890
Christophe Demko <chdemko@gmail.com>
2018-10-22 14:15:32 +0200

    Use phpbench

--- benchmarks/TreeMap.php
+++ benchmarks/TreeMap.php
@@ -1,152 +1,218 @@
 <?php
 
 /**
- * TreeMap beanchmark
+ * TreeMap benchmark class
  *
- * @package     SortedCollection
- * @subpackage  Map
+ * @author     Christophe Demko <chdemko@gmail.com>
+ * @copyright  Copyright (C) 2012-2018 Christophe Demko. All rights reserved.
  *
- * @author      Christophe Demko <chdemko@gmail.com>
- * @copyright   Copyright (C) 2012-2018 Christophe Demko. All rights reserved.
- *
- * @license     BSD 3-Clause License
+ * @license    BSD 3-Clause License
  *
  * This file is part of the php-sorted-collections package https://github.com/chdemko/php-sorted-collections
  */
 
-require __DIR__ . '/../vendor/autoload.php';
-
-use chdemko\SortedCollection\TreeMap;
-
-date_default_timezone_set('UTC');
-printf('TreeMap benchmarking run on ' . date('r') . PHP_EOL . PHP_EOL);
-printf('%25s %10s %10s %10s' . PHP_EOL, 'Operation', 'n', 'seconds', 'μs/(n log(n))');
-printf('-------------------------------------------------------------' . PHP_EOL);
-
-$tree = TreeMap::create();
-
-foreach (array(100, 1000, 10000, 100000) as $count)
+/**
+ * TreeMap benchmark class
+ *
+ * @since 1.0.5
+ */
+class TreeMap
 {
-	$start = microtime(true);
+	/**
+	 * @var TreeMap The tree map
+	 *
+	 * @since 1.0.5
+	 */
+	protected $tree;
 
-	for ($i = 0; $i < $count; $i++)
+	/**
+	 * @var SortedMap The sorted map
+	 *
+	 * @since 1.0.5
+	 */
+	protected $data;
+
+	/**
+	 * Provider for counts
+	 *
+	 * @return iterator Iterator on count
+	 *
+	 * @since 1.0.5
+	 */
+	public function provideCounts()
 	{
-		$tree[$i] = $i;
+		yield array('count' => 100);
+		yield array('count' => 1000);
+		yield array('count' => 10000);
+		yield array('count' => 100000);
 	}
 
-	$end = microtime(true);
+	/**
+	 * Provider for counts
+	 *
+	 * @return iterator Iterator on type
+	 *
+	 * @since 1.0.5
+	 */
+	public function provideTypes()
+	{
+		yield array('type' => 'tree');
+		yield array('type' => 'reversed');
+		yield array('type' => 'sub', 'from' => 0.30, 'to' => 0.70);
+		yield array('type' => 'sub', 'from' => 0.40, 'to' => 0.80);
+	}
 
-	printf(
-		'%25s %10d %10.2f %10.2f' . PHP_EOL,
-		'Insert all elements',
-		$count,
-		$end - $start,
-		($end - $start) / ($count * log($count)) * 1000000
-	);
+	/**
+	 * Create the tree map.
+	 *
+	 * @param array $params Array of parameters
+	 *
+	 * @return void
+	 *
+	 * @since 1.0.5
+	 */
+	public function init($params)
+	{
+		$this->tree = chdemko\SortedCollection\TreeMap::create();
+	}
 
-	$tree->clear();
+	/**
+	 * Create the sorted map.
+	 *
+	 * @param array $params Array of parameters
+	 *
+	 * @return void
+	 *
+	 * @since 1.0.5
+	 */
+	public function data($params)
+	{
+		if (isset($params['type']))
+		{
+			switch ($params['type'])
+			{
+				case 'tree':
+					$this->data = $this->tree;
+				break;
+				case 'reversed':
+					$this->data = chdemko\SortedCollection\ReversedMap::create($this->tree);
+				break;
+				case 'sub':
+					if (isset($params['from']) && isset($params['to']))
+					{
+						$this->data = chdemko\SortedCollection\SubMap::create(
+							$this->tree,
+							(int) ($params['from'] * $params['count']),
+							(int) ($params['to'] * $params['count'])
+						);
+					}
+					else
+					{
+						$this->data = chdemko\SortedCollection\SubMap::create(
+							$this->tree,
+							null,
+							null
+						);
+					}
+				break;
+			}
+		}
+		else
+		{
+			$this->data = $this->tree;
+		}
+	}
+
+	/**
+	 * Clear the tree map.
+	 *
+	 * @param array $params Array of parameters
+	 *
+	 * @return void
+	 *
+	 * @since 1.0.5
+	 */
+	public function finish($params)
+	{
+		$this->tree->clear();
+	}
+
+	/**
+	 * @BeforeMethods({"init", "data"})
+	 * @AfterMethods({"finish"})
+	 * @Revs(5)
+	 * @ParamProviders({"provideCounts"})
+	 *
+	 * @param array $params Array of parameters
+	 *
+	 * @return void
+	 *
+	 * @since 1.0.5
+	 */
+	public function benchFill($params)
+	{
+		for ($i = 0; $i < $params['count']; $i++)
+		{
+			$this->tree[$i] = $i;
+		}
+	}
+
+	/**
+	 * @BeforeMethods({"init", "benchFill", "data"})
+	 * @AfterMethods({"finish"})
+	 * @Revs(5)
+	 * @ParamProviders({"provideCounts", "provideTypes"})
+	 *
+	 * @param array $params Array of parameters
+	 *
+	 * @return void
+	 *
+	 * @since 1.0.5
+	 */
+	public function benchSearch($params)
+	{
+		if (isset($params['from']))
+		{
+			$min = (int) ($params['from'] * $params['count']);
+		}
+		else
+		{
+			$min = 0;
+		}
+
+		if (isset($params['to']))
+		{
+			$max = (int) ($params['to'] * $params['count']);
+		}
+		else
+		{
+			$max = $params['count'];
+		}
+
+		for ($i = $min; $i < $max; $i++)
+		{
+			$value = $this->data[$i];
+		}
+	}
+
+	/**
+	 * @BeforeMethods({"init", "benchFill", "data"})
+	 * @AfterMethods({"finish"})
+	 * @Revs(5)
+	 * @ParamProviders({"provideCounts"})
+	 *
+	 * @param array $params Array of parameters
+	 *
+	 * @return void
+	 *
+	 * @since 1.0.5
+	 */
+	public function benchClean($params)
+	{
+		for ($i = 0; $i < $params['count']; $i++)
+		{
+			unset($this->tree[$i]);
+		}
+	}
 }
 
-foreach (array(100, 1000, 10000, 100000) as $count)
-{
-	for ($i = 0; $i < $count; $i++)
-	{
-		$tree[$i] = $i;
-	}
-
-	$start = microtime(true);
-
-	for ($i = 0; $i < $count; $i++)
-	{
-		unset($tree[$i]);
-	}
-
-	$end = microtime(true);
-
-	printf(
-		'%25s %10d %10.2f %10.2f' . PHP_EOL,
-		'Remove all elements',
-		$count,
-		$end - $start,
-		($end - $start) / ($count * log($count)) * 1000000
-	);
-
-	$tree->clear();
-}
-
-foreach (array(100, 1000, 10000, 100000) as $count)
-{
-	for ($i = 0; $i < $count; $i++)
-	{
-		$tree[$i] = $i;
-	}
-
-	$start = microtime(true);
-
-	for ($i = 0; $i < $count; $i++)
-	{
-		$value = $tree[$i];
-	}
-
-	$end = microtime(true);
-
-	printf(
-		'%25s %10d %10.2f %10.2f' . PHP_EOL,
-		'Search all elements',
-		$count,
-		$end - $start,
-		($end - $start) / ($count * log($count)) * 1000000
-	);
-
-	$tree->clear();
-}
-
-foreach (array(100, 1000, 10000, 100000) as $count)
-{
-	for ($i = 0; $i < $count; $i++)
-	{
-		$tree[$i] = $i;
-	}
-
-	$start = microtime(true);
-
-	foreach ($tree as $key => $value)
-	{
-	}
-
-	$end = microtime(true);
-
-	printf(
-		'%25s %10d %10.2f %10.2f' . PHP_EOL,
-		'Loop on all elements',
-		$count,
-		$end - $start,
-		($end - $start) / ($count * log($count)) * 1000000
-	);
-
-	$tree->clear();
-}
-
-foreach (array(100, 1000, 10000, 100000) as $count)
-{
-	for ($i = 0; $i < $count; $i++)
-	{
-		$tree[$i] = $i;
-	}
-
-	$start = microtime(true);
-
-	$value = count($tree);
-
-	$end = microtime(true);
-
-	printf(
-		'%25s %10d %10.2f %10.2f' . PHP_EOL,
-		'Count all elements',
-		$count,
-		$end - $start,
-		($end - $start) / ($count * log($count)) * 1000000
-	);
-
-	$tree->clear();
-}
